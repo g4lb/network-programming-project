60f6d1d0096fc4eb7f09189c9b05650d54dd7cc9
bar <bardavvid@gmail.com>
2016-05-27 08:19:27 -0700

    changed MCA open session method

--- TCPMessengerServer/src/main.cpp
+++ TCPMessengerServer/src/main.cpp
@@ -12,8 +12,21 @@
 using namespace std;
 using namespace npl;
 
-int main() {
+
+void printInstructions(){
 	cout<<"Welcome to TCP Messenger Server"<<endl;
+	cout<<"Print the users list from server type: lu"<<endl;
+	cout<<"Print the connected users list type: lcu"<<endl;
+    cout<<"Print the sessions: ls"<<endl;
+	cout<<"Print available rooms type: lr"<<endl;
+	cout<<"Print users in room type: lru <room name>"<<endl;
+
+	cout<<"To exit type: x"<<endl;
+}
+
+int main() {
+	printInstructions();
+
 	MessengerServer* server = new MessengerServer();
 
 	while(true){
@@ -26,7 +39,22 @@ int main() {
 		}else if(command == "x"){
 			server->exit();
 			break;
-		}else{
+		}else if(command == "lu"){
+
+        }
+        else if(command == "lcu"){
+
+        }
+        else if(command == "ls"){
+
+        }
+        else if(command == "lr"){
+
+        }
+        else if(command == "lru"){
+
+        }
+        else{
 			cout<<"wrong input"<<endl;
 		}
 	}
